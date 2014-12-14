deaeb1d16d48863ed7c56e65a58b50365c9b56e7
Dor Tzur <dortzur@gmail.com>
2014-12-15 00:32:58 +0200

    secure random implemented on node

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 module.exports = function (options) {
-    function randomChar(chars) {
-        return chars[Math.round(Math.random() * (chars.length - 1))];
+
+    function secureRandom(length, chars) {
+        var crypto = require('crypto');
+        var result = "";
+        var randomBytes = crypto.randomBytes(length);
+        var index;
+        for (var i = 0; i < length; i++) {
+            index = randomBytes[i] % chars.length;
+            result += chars[index];
+        }
+        return result;
     }
+
+    function mathRandom(length, chars) {
+        var result = "";
+        var index;
+        for (var i = length; i > 0; --i) {
+            index = Math.round(Math.random() * (chars.length - 1));
+            result += chars[index];
+        }
+        return result;
+    }
+
+
 
     //defaults
     options = options || {};
@@ -9,9 +30,13 @@ module.exports = function (options) {
     options.chars = options.chars || '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     options.prefix = options.prefix || "";
     options.suffix = options.suffix || "";
-    var result = '';
-    for (var i = options.length; i > 0; --i) {
-        result += randomChar(options.chars);
+    options.secure = options.secure || true;
+
+    var result;
+    if (options.secure) {
+        result = secureRandom(options.length, options.chars);
+    } else {
+        result = mathRandom(options.length, options.chars);
     }
     return options.prefix + result + options.suffix;
 };
