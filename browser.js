289ba8861f6ed8e2ed83b9f79c890910f201958c
Dor Tzur <dortzur@gmail.com>
2015-05-23 12:58:02 +0300

    exposed "isSecureSupported"

--- browser.js
+++ browser.js
@@ -2,3 +2,4 @@ var secureContainer = require('./lib/browser_secure');
 
 var randomFactory = require("./lib/simpleRandom");
 module.exports = randomFactory.create(secureContainer);
+module.exports.isSecureSupported = secureContainer.isSupported;
--- dist/simple_random.js
+++ dist/simple_random.js
@@ -92,5 +92,6 @@ var secureContainer = require('./lib/browser_secure');
 
 var randomFactory = require("./lib/simpleRandom");
 module.exports = randomFactory.create(secureContainer);
+module.exports.isSecureSupported = secureContainer.isSupported;
 },{"./lib/browser_secure":1,"./lib/simpleRandom":2}]},{},[3])(3)
 });
