09541fdfea21b75f44f5f288ce5eb457aec5ab55
Dor Tzur <dortzur@gmail.com>
2015-05-23 13:49:33 +0300

    fixed digits and letters flags

--- test/options.js
+++ test/options.js
@@ -1,3 +1,7 @@
+var _digits = "0123456789";
+var _letters = "abcdefghijklmnopqrstuvwxyz";
+var _allLetters = _letters + _letters.toUpperCase();
+
 describe('options', function () {
     var chai = require('chai');
     var should = chai.should();
@@ -8,6 +12,25 @@ describe('options', function () {
     it('should have length of 32', function () {
         var random = simpleRandom({length: 32});
         random.should.have.length(32);
+    });
+
+    it('should have only digits', function () {
+        var random = simpleRandom({letters: false});
+        random.split("").forEach(function (char) {
+            _digits.indexOf(char).should.be.above(-1);
+        });
+    });
+    it('should have only letters', function () {
+        var random = simpleRandom({digits: false});
+        random.split("").forEach(function (char) {
+            _allLetters.indexOf(char).should.be.above(-1);
+        });
+    });
+    it('should have only lower case letters', function () {
+        var random = simpleRandom({digits: false, caseSensitive: false});
+        random.split("").forEach(function (char) {
+            _letters.indexOf(char).should.be.above(-1);
+        });
     });
     it('should start with _tmp_', function () {
         var random = simpleRandom({prefix: "_tmp_"});
@@ -26,8 +49,7 @@ describe('options', function () {
         var options = {chars: "1234567890a", length: 40, secure: true};
         var random = simpleRandom(options);
         random.should.have.lengthOf(options.length);
-        var randomArray = random.split("");
-        randomArray.forEach(function (char) {
+        random.split("").forEach(function (char) {
             options.chars.indexOf(char).should.be.above(-1);
         });
     });
