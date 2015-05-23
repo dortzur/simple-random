4ff59189513b6c42aaee8756039323c5eacacd31
Dor Tzur <dortzur@gmail.com>
2015-05-23 10:36:17 +0300

    Changed default to math random,  major version bump, added options to specify digits only, letters only, or case insensitive letters

--- index.js
+++ index.js
@@ -1,65 +1,13 @@
+var common = require("./lib/common");
+var secure = require('./lib/node_secure');
 module.exports = function (options) {
-
-    function nodeSecureRandom(length, chars) {
-        var crypto = require('crypto');
-        var randomBytes = crypto.randomBytes(length);
-
-        return bytesToChars(randomBytes, chars);
-    }
-
-    //function browserSecureRandom(length, chars) {
-    //    var isSupported = 'crypto' in window && !!window.crypto && 'getRandomValues' in window.crypto;
-    //    if (!isSupported) {
-    //        throw new Error("secure random is unsupported in this browser. " +
-    //        "You can change the \"secure\" option to false for pseudo-random values.");
-    //    }
-    //    var randomBytes = window.crypto.getRandomValues(new Uint8Array(length));
-    //    return bytesToChars(randomBytes, chars);
-    //}
-
-    function bytesToChars(byteArray, chars) {
-        var index;
-        var result = "";
-        for (var i = 0; i < byteArray.length; i++) {
-            index = byteArray[i] % chars.length;
-            result += chars[index];
-        }
-        return result;
-    }
-
-    function mathRandom(length, chars) {
-        var result = "";
-        var index;
-        for (var i = length; i > 0; --i) {
-            index = Math.round(Math.random() * (chars.length - 1));
-            result += chars[index];
-        }
-        return result;
-    }
-
-
-    var isNode = typeof module !== 'undefined' && module.exports;
-
-
-    //defaults
-    options = options || {};
-    options.length = options.length || 16;
-    options.chars = options.chars || '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    options.prefix = options.prefix || "";
-    options.suffix = options.suffix || "";
-    if (typeof options.secure === "undefined") {
-        options.secure = true;
-    }
-
+    options = common.initOptions(options);
     var result;
     if (options.secure) {
-        //if (isNode) {
-        result = nodeSecureRandom(options.length, options.chars);
-        //} else {
-        //    result = browserSecureRandom(options.length, options.chars);
-        //}
+        var randomBytes = secure.getRandomBytes(options.length);
+        result = common.bytesToChars(randomBytes, options.chars);
     } else {
-        result = mathRandom(options.length, options.chars);
+        result = common.mathRandom(options.length, options.chars);
     }
     return options.prefix + result + options.suffix;
 };
